06c00d53b44843b15565db3a7715b3c78cb74a93
Kaushal Nerkar <kaushalnerkar007@gmail.com>
2024-08-01 22:46:51 -0400

    script updated

--- end_to_end.groovy
+++ end_to_end.groovy
@@ -6,32 +6,35 @@ pipeline {
     }
     environment {
         KUBECONFIG = 'kubeconfig'
-        APP_NAME = ''
-        VERSION = ''
     }
   
     stages {
         stage('Checkout') {
             steps {
                 script {
-                   
+                
                     checkout([$class: 'GitSCM', 
                         branches: [[name: 'main']], 
                         userRemoteConfigs: [[url: 'https://github.com/kaushaln1/SpringBootApp.git']]
                     ])
 
+                    def appName = ''
+                    def version = ''
                     
-                    def appName = sh(script: '''
-                        grep -oPm1 "(?<=<name>)[^<]+" pom.xml || grep -oPm1 "(?<=\"name\": \")[^\"]+" package.json
-                    ''', returnStdout: true).trim().toLowerCase().replaceAll("\\s", "")
-
-                    def version = sh(script: '''
-                        grep -oPm1 "(?<=<version>)[^<]+" pom.xml || grep -oPm1 "(?<=\"version\": \")[^\"]+" package.json
-                    ''', returnStdout: true).trim()
-
-                    appName = appName.toLowerCase().replaceAll("\\s", "")
+                    if (fileExists('pom.xml')) {
+                        appName = sh(script: "mvn help:evaluate -Dexpression=project.name -q -DforceStdout", returnStdout: true).trim()
+                        version = sh(script: "mvn help:evaluate -Dexpression=project.version -q -DforceStdout", returnStdout: true).trim()
+                    } else if (fileExists('package.json')) {
+                        appName = sh(script: "jq -r .name package.json", returnStdout: true).trim()
+                        version = sh(script: "jq -r .version package.json", returnStdout: true).trim()
+                    }
                     
-                    env.APP_NAME = appName
+                    // Print extracted values for debugging
+                    echo "Extracted appName: ${appName}"
+                    echo "Extracted version: ${version}"
+                    
+                    // Set environment variables
+                    env.APP_NAME = appName.toLowerCase()
                     env.VERSION = version
                 }
             }
@@ -52,12 +55,13 @@ pipeline {
         stage('Sonar Scan') {
             environment {
                 scannerHome = tool 'SonarQubeScanner'
+                sonarProjectName = "${env.APP_NAME}"
             }
             steps {
                 withSonarQubeEnv(credentialsId: 'SonarQube', installationName: 'sonarCubeServer') {
-                    sh '''${scannerHome}/bin/sonar-scanner -Dsonar.projectName=${env.APP_NAME} \
+                    sh '''${scannerHome}/bin/sonar-scanner -Dsonar.projectName=${sonarProjectName} \
                           -Dsonar.java.binaries=. \
-                          -Dsonar.projectKey=${env.APP_NAME}'''
+                          -Dsonar.projectKey=${sonarProjectName}'''
                 }
             }
         }
@@ -79,7 +83,7 @@ pipeline {
             steps {
                 script {
                     sh "docker build -t ${env.APP_NAME}:${env.VERSION} ."
-                    sh "docker tag ${env.APP_NAME}:${env.VERSION} kauhsaln1/${env.APP_NAME}:latest"
+                    sh "docker tag ${env.APP_NAME}:${env.VERSION} kaushaln1/${env.APP_NAME}:latest"
                 }
             }
         }
@@ -88,7 +92,7 @@ pipeline {
             steps {
                 script {
                     def formatOption = "--format template --template \"@/usr/local/share/trivy/templates/html.tpl\""
-                    def imageFullName = "kauhsaln1/${env.APP_NAME}:latest"
+                    def imageFullName = "kaushaln1/${env.APP_NAME}:latest"
 
                     sh """
                     trivy image $imageFullName $formatOption --timeout 10m --output report.html || true
@@ -109,7 +113,7 @@ pipeline {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'dockerCreds', passwordVariable: 'dockerPass', usernameVariable: 'dockerUser')]) {
                     sh 'docker login -u ${dockerUser} --password ${dockerPass}'
-                    sh "docker push kauhsaln1/${env.APP_NAME}:latest"
+                    sh "docker push kaushaln1/${env.APP_NAME}:latest"
                 }
             }
         }
@@ -117,18 +121,20 @@ pipeline {
         stage('Deploy to Kubernetes-Helm') {
             steps {
                 script {
+             
                     
                     sh 'helm repo add kaushaln1 https://kaushaln1.github.io/helm_charts'
                     sh 'helm repo update'
-                    sh "helm install ${env.APP_NAME} kaushaln1/springboot --namespace jenkins-agent -f dev/values.yaml"
+                    // sh "helm install ${env.APP_NAME} kaushaln1/springboot --namespace jenkins-agent -f dev/values.yaml"
+                    
+                    withCredentials([file(credentialsId: 'kubeconfig', variable: 'config')]) {
+                          sh """
+                          export KUBECONFIG=\${config}
+                          helm install ${env.APP_NAME} kaushaln1/springboot --namespace jenkins-agent -f dev/values.yaml
+                          """
+        }
                 }
             }
         }
-    }
 
-    post {
-        always {
-            cleanWs()
-        }
-    }
 }
